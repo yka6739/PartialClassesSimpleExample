3c8b56e2cfa9c038e9847dbd36bdd702841f2fc3
marsal <marsal@DESKTOP-SL3HUAK>
2021-07-10 18:37:06 +0530

    Add project files.

---
+++ PartialClassesSimpleExample/Client/Pages/Index.razor.cs
@@ -0,0 +1,25 @@
+﻿using PartialClassesSimpleExample.Client.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialClassesSimpleExample.Client.Pages
+{
+    public partial class Index
+    {
+        private List<Data> d;
+        protected override void OnInitialized()
+        {
+            d = new List<Data>()
+            {
+                new Data(){Id=101,Name="Amit",Address="Noida"},
+                new Data(){Id=101,Name="Amit",Address="Noida"},
+                new Data(){Id=101,Name="Amit",Address="Noida"},
+                new Data(){Id=101,Name="Amit",Address="Noida"},
+                new Data(){Id=101,Name="Amit",Address="Noida"},
+                new Data(){Id=101,Name="Amit",Address="Noida"}
+            };
+        }
+    }
+}
---
+++ PartialClassesSimpleExample/Client/Shared/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialClassesSimpleExample.Client.Shared
+{
+    public class Data
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
